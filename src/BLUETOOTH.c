54e5d69fe22dda380302ef882d67281fb3f223bd
Mohamed <115455823+MohamedKhaleeed@users.noreply.github.com>
2022-10-11 19:36:24 +0200

    Add files via upload

---
+++ src/BLUETOOTH.c
@@ -0,0 +1,54 @@
+#include "BLUETOOTH.h"
+#include "avr/io.h" /* To use the UART Registers */
+#include "types_macros.h" /* To use the macros like SET_BIT */
+
+
+void UART_init(uint32 baud_rate)
+{
+	uint16 ubrr_value = 0;
+
+	UCSRA = (1<<U2X);
+
+
+	UCSRB = (1<<RXEN) | (1<<TXEN);
+
+
+	UCSRC = (1<<URSEL) | (1<<UCSZ0) | (1<<UCSZ1);
+
+	ubrr_value = (uint16)(((F_CPU / (baud_rate * 8UL))) - 1);
+
+	UBRRH = ubrr_value>>8;
+	UBRRL = ubrr_value;
+}
+
+
+void UART_sendByte(const uint8 data)
+{
+
+	while(BIT_IS_CLEAR(UCSRA,UDRE))
+	{}
+
+
+	UDR = data;
+
+}
+
+
+uint8 UART_recieveByte(void)
+{
+	while(BIT_IS_CLEAR(UCSRA,RXC)){}
+
+
+    return UDR;
+}
+
+void UART_SendString(char *str)
+{
+	unsigned char j=0;
+
+	while (str[j]!=0)		/* Send string till null */
+	{
+		UART_sendByte(str[j]);
+		j++;
+	}
+}
